159882544d721ba2c952ab9690698a860090a712
BluStudios2021 <97747417+BluStudios2021@users.noreply.github.com>
2022-01-19 23:30:07 +0100

    Update material_colors.py

--- material_colors.py
+++ material_colors.py
@@ -1,5 +1,16 @@
 '''
     A simple implementation of the material design colors in python
+     - currently only supports 'rgb' format; more will be added soon
+    
+    Usage:
+
+    from material_colors import MaterialColors
+    
+    Colors = MaterialColors('rgb')
+    
+    my_red_color = Colors.red()
+
+    my_purple_color = Colors.purple(300)
 '''
 
 class MaterialColors():
